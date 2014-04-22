3759cbf44701ab0dfae9fca6d15bae99c5bc22ee
Marcus Povey <marcus@marcus-povey.co.uk>
2014-04-22 17:48:02 +0100

    Using OpenPGP.js to generate new key pair (rather than use up server entropy)

--- OpenPGPSignin/Assets/OpenPGPSigninAccount.js
+++ OpenPGPSignin/Assets/OpenPGPSigninAccount.js
@@ -6,12 +6,16 @@ $(document).ready(function(){
    $('#generate').click(function(event){
 	var openpgp = window.openpgp;
 	
-	if (($('#public_key').val() == "") && ($('#private_key').val() == "")) { 
-	    
-	    key = openpgp.generateKeyPair(1, 2048, $('#pgp-keys-userid').val(), '');
-	    
-	    $('#public_key').val(key.publicKeyArmored);
-	    $('#private_key').val(key.privateKeyArmored);
+	$(this).text('Generating...');
+
+	key = openpgp.generateKeyPair(1, 2048, $('#pgp-keys-userid').val(), '');
+
+	$('#public_key').val(key.publicKeyArmored);
+	$('#private_key').val(key.privateKeyArmored);
+
+	
+	if (($('#public_key').val() != "") && ($('#private_key').val() != "")) { 
+	    $(this).fadeOut();
 	}
    });
    
