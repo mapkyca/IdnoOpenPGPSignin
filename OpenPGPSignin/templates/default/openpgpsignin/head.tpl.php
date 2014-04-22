9451445d69bee5b08ddea592ed2e682be431c194
Marcus Povey <marcus@marcus-povey.co.uk>
2014-04-22 10:04:53 +0100

    Using Link header

--- OpenPGPSignin/templates/default/openpgpsignin/head.tpl.php
+++ OpenPGPSignin/templates/default/openpgpsignin/head.tpl.php
@@ -1,8 +1,13 @@
 <?php
     $currentpage = \Idno\Core\site()->currentPage();
+    
     if ($currentpage->matchUrl('/profile/([A-Za-z0-9]+)/?')) {
+	$url = $currentpage->currentUrl();
+	$url = trim($url, '/ ') . '/';
+	
+	header("Link: <{$url}publickey.asc>; rel=\"key\"", false);
 	?>
-<link href="<?= $currentpage->currentUrl(); ?>publickey.asc" rel="key" />	
+<link href="<?= $url; ?>publickey.asc" rel="key" />	
 	<?php
     }
     
