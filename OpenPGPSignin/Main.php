c21b9cb2b48957605ba0d27c950f526a61bb3939
Marcus Povey <marcus@marcus-povey.co.uk>
2014-04-23 20:01:05 +0100

    On follow key import

--- OpenPGPSignin/Main.php
+++ OpenPGPSignin/Main.php
@@ -10,9 +10,79 @@
 	     * Failing that it'll look for a class="key" block on the page.
 	     * 
 	     * @param type $url
+	     * @return string|false Will return the new key's fingerprint or FALSE
 	     */
 	    private function findPublicKey($url) {
-		// TODO
+		
+		if ($page = file_get_contents($url)) {
+		    
+		    $endpoint_url = null;
+		    
+		    
+		    // Get headers from request
+		    $headers = $http_response_header;
+		    
+		    
+		    // See if we have an endpoint in header
+		    foreach ($headers as $header) {
+			if ((preg_match('~<(https?://[^>]+)>; rel="key"~', $header, $match)) && (!$endpoint_url)) {
+			    $endpoint_url = $match[1];
+			    error_log("Found endpoint URL <{$endpoint_url}> in HTTP header");
+			}
+		    }
+		    
+		    // Nope, so see if we have it in meta
+		    if (!$endpoint_url) {
+			if (preg_match('/<link href="([^"]+)" rel="key" ?\/?>/i', $page, $match)) {
+			    $endpoint_url = $match[1];
+			    error_log("Found endpoint URL <{$endpoint_url}> in meta");
+			}
+		    }
+		    
+		    // Still nope, see if we've linked to it in page
+		    if (!$endpoint_url) {
+			if (preg_match('/<a href="([^"]+)" rel="key" ?\/?>/i', $page, $match)) {
+			    $endpoint_url = $match[1];
+			    error_log("Found endpoint URL <{$endpoint_url}> in a link on the page");
+			}
+		    }
+		    if (!$endpoint_url) {
+			if (preg_match('/<a rel="key" href="([^"]+)" ?\/?>/i', $page, $match)) {
+			    $endpoint_url = $match[1];
+			    error_log("Found endpoint URL <{$endpoint_url}> in a link on the page");
+			}
+		    }
+		    
+		    $key = null;
+		    if ($endpoint_url) {
+			// Yes, we have an endpoint URL, go get the key data
+			error_log("Retrieving key data...");
+			$key = trim(file_get_contents($endpoint_url));
+			error_log("Key is $key");
+		    }
+		    
+		    // If no key data, try and find key data within a classed block on the page
+		    if (!$key) {
+			if (preg_match('/<[^\s]+ class="[^"]*key[^"]*">([^<]*)/im', $page, $match)) {
+			    error_log("Still no key data, looking on the page...");
+			    $key = $match[1];
+			}
+		    }
+		    
+		    // We have some key data, try and use it!
+		    if ($key) {
+			error_log("Some key data was found...");
+			return $key;
+			
+		    }
+		    
+		    
+		    error_log("No key data found :(");
+		}
+		else
+		    error_log("Could not load $url");
+		
+		return false;
 	    }
 	    
             function registerPages() {
@@ -28,7 +98,34 @@
 		\Idno\Core\site()->template()->extendTemplate('account/menu/items', 'openpgpsignin/account/menu');
 		\Idno\Core\site()->addPageHandler('account/pgpkeys', '\IdnoPlugins\OpenPGPSignin\Pages\Account');
 		
-		
+		// When a friend is added, we want to retrieve their keys
+		\Idno\Core\site()->addEventHook('follow', function(\Idno\Core\Event $event) {
+
+                    $user = $event->data()['following'];
+
+                    if ($user instanceof User) {
+                        
+			if ($publickey = $this->findPublicKey($user->getURL())) {
+			    // We found a public key here, so...
+			    
+			    
+			    // Save it to the keyring
+			    $gpg = new \gnupg();
+			    $result = $gpg->import($public_key);
+
+			    // Save a signature against the user
+			    if ($result && isset($result['fingerprint'])) {
+				error_log("Imported public key, with fingerprint {$result['fingerprint']}");
+
+				$user->pgp_publickey_fingerprint = $result['fingerprint'];
+				$user->save();
+			    }
+			    
+			}
+			
+                    }
+
+                });
 		
 		
 		// Extend private denied page (may require upstream change)
